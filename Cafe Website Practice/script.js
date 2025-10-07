f4f9a2b55307575229a5bc5524005441764d5f3a
Pujan Shrestha <pujanshrestha045@gmail.com>
2025-10-07 23:49:27 +0545

    commit

--- Cafe Website Practice/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const mobileNav = document.getElementById('mobileNav');
-
-hamburger.addEventListener('click',() => {
-    mobileNav.classList.toggle('active');
-});
