cb8c457cf1f0ac2eff106508a676d0fdf94f075c
Pujan Shrestha <pujanshrestha045@gmail.com>
2025-10-12 21:52:30 +0545

    mobile nav toggle fix

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
         const hamburger = document.getElementById('hamburger');
         const mobileNav = document.getElementById('mobileNav');
+        const navigation= document.getElementById('navigation');
 
         hamburger.addEventListener('click', () => {
             mobileNav.classList.toggle('active');
+            navigation.classList.toggle('solid')
         });
     });
